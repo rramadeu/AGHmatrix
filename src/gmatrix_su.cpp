8058fc29672813531f5175762fe01b03fd9cd5b9
Rodrigo R Amadeu <rramadeu@gmail.com>
2026-07-21 23:20:46 -0500

    Add license and description comments to gmatrix_su.cpp
    Added licensing information and file description.

--- src/gmatrix_su.cpp
+++ src/gmatrix_su.cpp
@@ -1,3 +1,31 @@
+/*
+ AGHmatrix: Relationship Matrices for Diploid and Autopolyploid Species
+ Copyright (C) 2016-2026 Rodrigo Amadeu
+
+ This file is part of AGHmatrix.
+
+AGHmatrix is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+
+ AGHmatrix is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+/*
+ File: gmatrix_su.cpp
+ Description: implement gmatrix su method
+
+ Written by Thiago de Paula Oliveira, 2025
+ */
+
 // Thiago de Paula Oliveira
 // gmatrix_su.cpp
 // [[Rcpp::depends(RcppArmadillo)]]
